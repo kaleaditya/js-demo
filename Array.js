c859fce8910a81607780a79f62f3d2db41a4cade
kaleaditya <Akale203@gmail.com>
2020-09-10 13:27:24 +0530

    js project 1

---
+++ Array.js
@@ -0,0 +1,3 @@
+
+let selectColor =['red','green'] ;
+console.log(selectColor[0]);
---
+++ index.js
@@ -0,0 +1,11 @@
+// person is object & the valiables are name and age.
+let person = {
+    name:'aditya',
+    age: 22
+};
+
+//in js let is used if u want to change the value of object and to put the value constant the keyword is cons.
+
+// to change the name the best option is DOT NOTATION 
+person.name = 'diya'
+ console.log(person);
